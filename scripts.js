b0702e9606a93cfeba785525c7e34d1f0279690a
zak10110 <35228898+zak10110@users.noreply.github.com>
2022-12-15 04:58:37 +0300

    Some HTML and JS update
    Add new enter field and also add script to change text in this label by clicking on button

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function gifapears(){
 
-    document.getElementById('bar_block').style.display='flex'
-
+    document.getElementById('bar_block').style.display='flex';
+    document.getElementById('resullabel').textContent='КУ, Дарова, просто для проверки =)';
 
 }
