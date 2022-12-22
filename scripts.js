a5d7743022077fa7ca36e33f0179423bae7159a5
zak10110 <35228898+zak10110@users.noreply.github.com>
2022-12-22 09:38:23 +0300

    JS Update
    Try to make stopping distance formula

--- scripts.js
+++ scripts.js
@@ -1,6 +1,35 @@
-function gifapears(){
 
+
+
+function gifapears(){
+    var s=0;
+    var kc=0;
+    var v=0;
+    v=Number(document.getElementById("enter").textContent);
+    var select = document.getElementById("slroad");
+    var value = select.value;
+    if(value=="Сухой асфальт")
+    {
+        kc=0.8;
+    }
+    else if(value=="Мокрая дорога")
+    {
+        kc=0.55;
+
+    }
+    else if(value=="Снег")
+    {
+        kc=0.4;
+
+    }
+    else if(value=="Гололёд")
+    {
+        kc=0.25;
+
+    }
+
+    s=Math.pow(v,2)/(254* parseFloat(kc));
     document.getElementById('bar_block').style.display='flex';
-    document.getElementById('resullabel').textContent='КУ, Дарова, просто для проверки =)';
+    document.getElementById('resullabel').textContent=`Тормозной путь:${s}м`;
 
 }
